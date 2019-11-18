a99460fb0325127b94366b22351c4142661fe127
kakeru <okinawan.kkr0713@icloud.com>
2019-11-18 15:23:33 +0900

    befor_create追記

--- app/models/create_age_at_callback.rb
+++ app/models/create_age_at_callback.rb
@@ -1,2 +1,6 @@
 class CreateAgeAtCallback < ApplicationRecord
+  
+  def before_create(person)
+    person.age = 18 if person.age.blank?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_091754) do
+ActiveRecord::Schema.define(version: 2019_11_18_061706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
