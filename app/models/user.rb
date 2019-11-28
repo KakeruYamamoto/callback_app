999c1a4cec2a190549641270071ede46de9e6df9
kakeru <okinawan.kkr0713@icloud.com>
2019-11-28 13:54:30 +0900

    終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,31 @@
 class User < ApplicationRecord
+  before_create :ensure_has_name
+  before_create CreateAgeAtCallback.new
+
+
+
+  before_save do
+    puts '１番目に呼ばれたコールバック'
+  end
+
+  before_save do
+    puts '２番目に呼ばれたコールバック'
+  end
+  # before_create do
+  #     self.name = 'Taro' if name.blank?
+  # end
+
+# #after_initializeは、Active Recordのオブジェクトがインスタンス化されるたびに呼び出されます
+#   after_initialize do |user|
+#     p user
+#     puts "オブジェクトを生成しました！"
+#     user.name = "Taro"
+#   end
+#
+#   private
+#
+#  def ensure_has_name
+#    puts "nameの値をTaroに設定します！"
+#    self.name = 'Taro' if name.blank?
+#  end
 end
