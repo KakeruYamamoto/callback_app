e29b96b1b0f4cff29679c5f733a9a258856e1bed
kakeru <okinawan.kkr0713@icloud.com>
2019-11-28 13:25:42 +0900

    モデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,2 @@
 class User < ApplicationRecord
-  before_create :ensure_has_name
-
-#after_initializeは、Active Recordのオブジェクトがインスタンス化されるたびに呼び出されます。
-  after_initialize do |user|
-    p user
-    puts "オブジェクトを生成しました！"
-    user.name = "Taro"
-  end
-
-  private
-
-  def ensure_has_name
-    puts "nameの値をTaroに設定します！"
-    self.name = 'Taro' if name.blank?
-  end
 end
